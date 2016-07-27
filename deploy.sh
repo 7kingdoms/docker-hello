b8ea9799f754c2d91b90ffcab3e30390313577c5
gitranger <lordhackalot@gmail.com>
2016-07-27 17:29:15 +0700

    configure aws region

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,9 @@ docker push 7kingdoms/hello:$SHA1
 ## Create new Elastic Beanstalk version
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws configure set default.region us-east-1
+#aws configure set default.output json
+
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name hello \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
